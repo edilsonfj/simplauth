2d4f051b7ceedcdd34af2664fa3fec4ffb93dacf
edilsonfj <edilson.desenvolved@gmail.com>
2024-08-25 18:07:31 -0300

    feat: added frontend structure
    Criado a estrutura base para desenvolviemnto da interface.

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Kanit as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  weight: ["500", "600"],
+});
+
+export const metadata: Metadata = {
+  title: "Simplauth",
+  description: "Projeto de api de autenticação.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
